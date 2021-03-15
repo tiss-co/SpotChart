770c23b19b1502a10398cf089fde89fadfb48c0c
amirhoseinmrn <amirhoseinmrn1@gmail.com>
2021-03-15 12:59:40 +0330

    Initial Commit

---
+++ SpotChart/SpotChart.h
@@ -0,0 +1,18 @@
+//
+//  SpotChart.h
+//  SpotChart
+//
+//  Created by Amir on 3/15/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpotChart.
+FOUNDATION_EXPORT double SpotChartVersionNumber;
+
+//! Project version string for SpotChart.
+FOUNDATION_EXPORT const unsigned char SpotChartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpotChart/PublicHeader.h>
+
+
